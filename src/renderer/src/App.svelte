666ca237acc52160fca8095c7c740117f29d545e
kmaksut <kmaksut32@gmail.com>
2024-06-01 23:56:04 +0300

    added appBadge

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,11 +1,21 @@
 <script>
 // @ts-nocheck
 
-  import { mainTodoActive, doneTodoActive, starsTodoActive } from './store/stores'
+  import { mainTodoActive, doneTodoActive, starsTodoActive, secretTodoActive, todos, optionsActive } from './store/stores'
   import Sidebar from './components/sidebar/Sidebar.svelte'
   import TodoList from './components/sidebar/routes/TodoList.svelte'
   import DoneList from './components/sidebar/routes/DoneList.svelte'
   import StarsList from './components/sidebar/routes/StarsList.svelte'
+  import SecretList from './components/sidebar/routes/SecretList.svelte'
+  import Options from './components/sidebar/routes/Options.svelte'
+  import { onMount } from 'svelte'
+
+  onMount(async () => {
+    const savedTodos = await window.api.loadTodos();
+    await window.api.system()
+    todos.set(savedTodos);
+  })
+  $: navigator.setAppBadge($todos.length)
 </script>
 
 <div class="container">
@@ -16,6 +26,8 @@
     <DoneList />
   {:else if $starsTodoActive}
     <StarsList />
+  {:else if $secretTodoActive}
+    <SecretList />
   {/if}
 </div>
 
