3e1f5bc30a33894f0eb406031a555371ddc90dd0
kmaksut <kmaksut32@gmail.com>
2024-06-01 23:43:34 +0300

    remove 'optimize deps'

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['uuid']
-  },
   main: {
     plugins: [externalizeDepsPlugin()]
   },
