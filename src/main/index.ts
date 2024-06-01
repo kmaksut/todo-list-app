c0018d19280a118b556c459bb84cc56c3f20690c
kmaksut <kmaksut32@gmail.com>
2024-06-01 23:53:59 +0300

    update

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,23 +1,23 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { app, shell, BrowserWindow, ipcMain, nativeTheme } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
-import { autoUpdater } from 'electron-updater'
+import fs from 'fs'
+
+const cfgLoc = join(app.getPath('userData'), 'todos', 'todo.json');
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1020,
     height: 720,
     resizable: false,
-    show: false, 
+    show: false,    
     autoHideMenuBar: true,
     fullscreenable: false,
-    ...(process.platform === 'linux' ? { icon } : {}),
-    webPreferences: {
+    webPreferences: { 
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
     }
-  })
+  })  
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
@@ -25,17 +25,41 @@ function createWindow(): void {
     shell.openExternal(details.url)
     return { action: 'deny' }
   })
-  ipcMain.on('get-cookies', (_event ,args)=> {
-    mainWindow.webContents.send('get-cook-renderer', args)
-    console.log(args); 
-  })
-  autoUpdater.on('update-available', (info) => {
-    mainWindow.webContents.send('update-available', info);
-    })
+  // IPC Functions
+  function saveTodos(todos) {
+    fs.writeFileSync(cfgLoc, JSON.stringify(todos));
+  }
   
-  autoUpdater.on('update-downloaded', (info) => {
-    mainWindow.webContents.send('update-downloaded', info);
-    })
+  function loadTodos() {
+    if (fs.existsSync(cfgLoc)) {
+      const data = fs.readFileSync(cfgLoc);
+      //@ts-ignore
+      return JSON.parse(data);
+    }
+    return [];
+  }
+  // IPC Proccess
+  ipcMain.handle('load-todos', () => { 
+    return loadTodos();
+  });
+  
+  ipcMain.handle('save-todos', (_event, todos) => {
+    saveTodos(todos);
+  });
+
+  ipcMain.handle('dark-mode:toggle', () => { 
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+  
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'light'
+  })
+  
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -47,14 +71,6 @@ app.whenReady().then(() => {
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
-  if (!app.isPackaged) {
-    autoUpdater.autoDownload = false;
-    autoUpdater.checkForUpdates();
-  } else {
-    autoUpdater.checkForUpdatesAndNotify();
-  }
-  // IPC test
-  // ipcMain.on('ping', () => console.log('pong'))
   createWindow()
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
@@ -64,5 +80,4 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-
 })
